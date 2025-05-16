58d019dac5accb15dc5862f9e40d322857cee5fe
Hunter Hockman <84150506+Anti-Loli@users.noreply.github.com>
2025-05-16 00:40:44 -0400

    Discarding functionality added

--- Scroundel/Assets/Scripts/CardInstance.cs
+++ Scroundel/Assets/Scripts/CardInstance.cs
@@ -1,11 +1,15 @@
+using System;
 using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CardInstance : MonoBehaviour
 {
-    public Card template; 
+    public Card template;
 
-    public void Init (Card data)
+    public event Action<CardInstance> DiscardCard;
+
+    public void Init(Card data)
     {
         template = data;
         GetComponent<SpriteRenderer>().sprite = data.cardArt;
@@ -14,10 +18,16 @@ public class CardInstance : MonoBehaviour
     public CardSuite GetCardSuite()
     {
         return template.suite;
-    } 
+    }
 
     public int GetCardValue()
     {
         return template.value;
+    }
+
+    void OnMouseDown()
+    {
+        DiscardCard.Invoke(this);
+        Destroy(gameObject);
     }
 }
--- Scroundel/Assets/Scripts/Room.cs
+++ Scroundel/Assets/Scripts/Room.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using static CardInstance;
 
 public class Room : MonoBehaviour
 {
@@ -8,17 +9,17 @@ public class Room : MonoBehaviour
     [SerializeField] List<Vector2> spawnPoints = new List<Vector2>();
     [SerializeField] private GameObject cardPrefab;
 
-    private  List<Card> cardList = new List<Card>(); //card data of the current room
-    private  List<CardInstance> roomList = new List<CardInstance>(); //card list for current room
+    private List<Card> cardList = new List<Card>(); //card data of the current room
+    private List<CardInstance> roomList = new List<CardInstance>(); //card list for current room
 
     void Awake()
     {
-        if(deck == null)
+        if (deck == null)
         {
             Debug.Log("Deck component not found after Inspector assignment");
             deck = GameObject.Find("Deck").GetComponent<Deck>();
 
-             if(deck == null)
+            if (deck == null)
             {
                 Debug.Log("Deck component not found after Getcomponent");
             }
@@ -33,12 +34,12 @@ public class Room : MonoBehaviour
 
     void Update()
     {
-        
+
     }
 
     void SpawnRoom()
     {
-        for(int i = 0; i < cardList.Count; i++)
+        for (int i = 0; i < cardList.Count; i++)
         {
             Vector2 spawnPos = (i < spawnPoints.Count) ? spawnPoints[i] : Vector2.zero;
 
@@ -48,7 +49,28 @@ public class Room : MonoBehaviour
 
             cardInstance.Init(cardList[i]);
 
+            cardInstance.DiscardCard += DiscardCard;
+
             roomList.Add(cardInstance);
         }
+    }
+
+
+    public void DiscardCard(CardInstance card)
+    {
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            if (card == roomList[i])
+            {
+                roomList.RemoveAt(i);
+                cardList.RemoveAt(i);
+            }
+        }
+
+        //  for (int i = 0; i < roomList.Count; i++)
+        // {
+        //     Debug.Log("Roomlist card " + i + ": " + roomList[i].GetCardSuite() + " " + roomList[i].GetCardValue());
+        //     Debug.Log("Cardlist card " + i + ": " + cardList[i].suite + " " + cardList[i].value);
+        // }
     }
 }
